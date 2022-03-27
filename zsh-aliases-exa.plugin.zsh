9720ada97af8abdabd714068126e48c586298c53
Rich Purnell <richpurnell@163.com>
2022-03-28 04:50:12 +0800

    feat: add alias for exa

---
+++ zsh-aliases-exa.plugin.zsh
@@ -0,0 +1,9 @@
+# general use
+alias l='exa -lbFa --time-style=long-iso'
+alias ll='exa -lbF --time-style=long-iso'
+alias llm='exa -lbFa --git --sort=modified --time-style=long-iso'
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
+
+# speciality views
+alias lt='exa --tree --level=2'
